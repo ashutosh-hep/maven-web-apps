a0c217b36324c6d052a946f28b67efc13e56c629
Ashutosh Kumar <ashutosh@Ashutoshs-MacBook-Air.local>
2024-10-17 19:36:58 +0530

    up

---
+++ spring-boot-demo/src/main/java/com/ashut/spring_boot_demo/SpringBootDemoApplication.java
@@ -0,0 +1,15 @@
+package com.ashut.spring_boot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+}
+
+
